4f4ff814d1270f6b361dfb2d8b580499043d2154
Bryan Gill <bryan.gill@gmail.com>
2018-03-08 12:56:51 -0600

    Refactoring
    Removed CSS, and updated package.json dependencies

--- src/components/brand/brand.vue
+++ src/components/brand/brand.vue
@@ -2,7 +2,7 @@
   <div id="mogul-brand" class="level">
     <div class="level-left">
       <div class="level-item">
-        <div class="brand-title">{{ msg }}</div>
+        <div class="is-size-5 has-text-weight-bold">{{ msg }}</div>
       </div>
     </div>
     <div class="level-right">
@@ -13,7 +13,7 @@
         <!--<router-link to="/players">Players</router-link>-->
       </div>
       <div class="level-item">
-        <span class="subtitle is-size-7">Latest Sim: Year: {{ details[0].simYear }}, Number: {{ details[0].simNumber }}</span>
+        <span><p class="is-italic is-size-7">Latest Sim: Year: {{ details[0].simYear }}, Number: {{ details[0].simNumber }}</p></span>
       </div>
     </div>
 
@@ -40,6 +40,20 @@
     },
     methods: {
       getCurrentSimulation: function () {
+        /* eslint-disable */
+        this.details = [
+          {
+            "simYear": "2078",
+            "simNumber": "8",
+            "realLifeSimTime": "1519697531003",
+            "realLifeSimDate": "Mon Feb 26 2018 20:12:11 GMT-0600 (CST)",
+            "createdAt": "2018-02-27T02:12:24.148Z",
+            "updatedAt": "2018-02-27T02:12:24.148Z",
+            "id": "5a94be88dfcaf8a3070b22dd"
+          }
+        ]
+//        this.$root.$emit('simDetails', this.details)
+
         let data = '/simulation/getDetails'
         let url = process.env.LOCAL_HOST + data
         fetch(url)
